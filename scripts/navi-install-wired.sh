fbd5e19d6927ff3a79cea8bafd0165b6c74c2023
rav3ndust <35274771+rav3ndust@users.noreply.github.com>
2026-04-29 05:02:51 -0500

    Update navi-install-wired.sh with TODO comments
    Add comments indicating future refactoring plans.

--- scripts/navi-install-wired.sh
+++ scripts/navi-install-wired.sh
@@ -2,7 +2,10 @@
 # ( navi-install-wired )
 # installs the wiredWM environment and all of its utilities
 # also configures all config files and builds/installations
+# ____________________________________________________________
 # TODO ! some of how this is handled is going to be refactored soon
+# TODO ! we will probably end up moving more of the logic from that script into this one for consolidation
+# ____________________________________________________________
 set -euo pipefail
 repo="https://github.com/rav3ndust/wiredWM"
 git clone "$repo"
