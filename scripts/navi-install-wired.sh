7ada623997f644b282abc6801a96d026ff87ff3e
rav3ndust <35274771+rav3ndust@users.noreply.github.com>
2026-04-29 05:00:21 -0500

    Create navi-install-wired.sh

---
+++ scripts/navi-install-wired.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# navi-install-wired
+# installs the wiredWM environment and all of its utilities
+# also configures all config files and builds/installations
+# TODO ! some of how this is handled is going to be refactored soon
+set -euo pipefail
+repo="https://github.com/rav3ndust/wiredWM"
+git clone "$repo"
+cd wiredWM/scripts-config
+chmod +x installer.sh; bash installer.sh &
