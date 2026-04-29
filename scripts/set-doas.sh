ab25dfd0b5c0c03a82198574fc0a4612b8ec6b0a
rav3ndust <35274771+rav3ndust@users.noreply.github.com>
2026-04-29 04:14:58 -0500

    Add script to install and configure doas

---
+++ scripts/set-doas.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# set-doas
+# installs and configures doas - we prefer it over sudo
+doas_conf="/etc/doas.conf"
+sudo apt update && sudo apt install opendoas -y
+sudo touch "$doas_conf"
+echo "permit persist "$USER" as root" | sudo tee "$doas_conf"
+exit
